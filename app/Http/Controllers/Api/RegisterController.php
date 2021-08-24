21f2ec0a4e837fb42a305dffbb8313dabd7df24e
Maciej <maciej.z@smartconcepts.pl>
2021-08-24 14:59:18 +0200

    fixing create user

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -74,6 +74,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'surname' => $data['surname'],
+            'birthdate' => $data['birthdate'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
