199a013f7c57e67c569c757753b903b3c7a0b226
Maciej <maciej.z@smartconcepts.pl>
2021-08-24 09:15:53 +0200

    no email verify

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -129,7 +129,8 @@ class RegisterController extends Controller
     {
         $this->validator($request->all())->validate();
 
-        event(new Registered($user = $this->create($request->all())));
+        // event(new Registered($user = $this->create($request->all())));
+        $user = $this->create($request->all());
 
         $this->guard()->login($user);
 
